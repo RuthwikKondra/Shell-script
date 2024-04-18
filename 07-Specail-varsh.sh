576095ede65f7c2fc671f857df0299f33c8925e4
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-18 22:10:19 +0530

    added file

--- 07-Specail-varsh.sh
+++ 07-Specail-varsh.sh
@@ -3,3 +3,8 @@
 echo "All variables: $@"
 echo "Number of variables passed:$#"
 echo "Script name: $0"
+echo "Curremt working Directory : $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running  this script: $USER"
+echo "Name of the host: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
