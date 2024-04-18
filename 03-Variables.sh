91cb1846bf2dad22efd8677460fbcc928238bfed
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-18 09:18:23 +0530

    declaring variables

---
+++ 03-Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#declaring of variables
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine .How are you?"
+echo "$PERSON1:: I am fine to. how is your work?"
+echo "$PERSON2:: not bad.I am thinking to upgrade to Devops"
