5004813472684a4c4da141f4b608120a969591c0
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-18 22:16:08 +0530

    added file

--- 07-Specail-varsh.sh
+++ 07-Specail-varsh.sh
@@ -9,3 +9,4 @@ echo "Which user is running  this script: $USER"
 echo "Name of the host: $HOSTNAME"
 echo "Process ID of the current shell script: $$"
 sleep 60
+echo "Process ID of last background command: $!"
