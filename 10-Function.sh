db774951b5a1c4e1155cdb81b2813ec8f230fbf6
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-20 18:01:51 +0530

    updated

--- 10-Function.sh
+++ 10-Function.sh
@@ -24,12 +24,12 @@ else
     echo "You are super user."
 fi     
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
 
 VALIDATE $? "Installing mysql"
 
 
-dnf install git -y
+dnf install git -y &>>$LOGFILE
 
 VALIDATE $? "Installing git"
 
