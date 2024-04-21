018a81cbbb9fbe463ef30e4b99db50c216ee35da
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-21 14:56:02 +0530

    modified

--- 13-Packages.sh
+++ 13-Packages.sh
@@ -16,8 +16,8 @@ fi
 
 for i in $@
 do  
-   echo "Packages to Install: $i"
-   dnf list installed $i &>>$LOGFILE
+   echo "package to install: $i"
+   dnf list installed $i &>>$LOGFILE 
    if [ $? -ne 0 ]
    then 
         echo "Already installed..skipping"
