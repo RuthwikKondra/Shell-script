353deb8ed9d06471f147899f17de95274538338a
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-21 14:50:41 +0530

    modified

--- 13-Packages.sh
+++ 13-Packages.sh
@@ -15,7 +15,8 @@ do
    dnf list installed $i &>>$LOGFILE
    if [ $? -ne 0]
    then 
-        echo "Already Installed..skipping:
+        echo "Already installed..skipping"
+    else
+        echo "$i not installed..Need to install"
     fi
-
 done
