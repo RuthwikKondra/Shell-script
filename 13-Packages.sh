c74416223f62a109a3bb4f5970705cf6b7e99b42
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-21 15:08:23 +0530

    modified

--- 13-Packages.sh
+++ 13-Packages.sh
@@ -9,22 +9,33 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
-     echo "Please run this script with root access"
-     exit 1 # manually exist if error comes
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are super user"
-fi 
+    echo "You are super user."
+fi
 
 for i in $@
-do  
+do
     echo "package to install: $i"
-    dnf list installed $i &>>$LOGFILE 
-    if [ $? -ne 0 ]
-    then 
-        echo  -e "$i Already installed.. $Y skipping $N"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed...$Y SKIPPING $N"
     else
-        echo "$i not installed..Need to install"
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
     fi
 done
