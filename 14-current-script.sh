a0de7a35487b90e85b4391095e0b046cf82f3e88
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-04-24 17:29:42 +0530

    modified

---
+++ 14-current-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+COURSE="Devops from Current Script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+# /15-other-script
+
+echo "After calling other script, course: $COURSE"
+
